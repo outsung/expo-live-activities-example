d7726112d7f3f575fda2b5180580208167142bcb
Junseo Oh <insung9546@gmail.com>
2024-11-01 14:24:27 +0900

    add `widgetExtension`

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,6 +10,24 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="startActivity" onPress={startActivity} />
     </View>
   );
+}
+
+import * as ReactNativeWidgetExtension from "react-native-widget-extension";
+
+function startActivity() {
+  if (ReactNativeWidgetExtension.areActivitiesEnabled()) {
+    const quarter = 3;
+    const scoreLeft = 1;
+    const scoreRight = 6;
+    const bottomText = "드가자";
+    ReactNativeWidgetExtension.startActivity(
+      quarter,
+      scoreLeft,
+      scoreRight,
+      bottomText
+    );
+  }
 }
